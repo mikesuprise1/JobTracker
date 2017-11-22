d9112c13ec498bea13e560f3364eba9849070a00
mikesuprise1 <mikesupris1@gmail.com>
2017-11-22 14:14:23 -0700

    Creating view pages and routes for those pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,18 @@
 class StaticPagesController < ApplicationController
     def home
     end
+
+    def builder
+    end
+
+    def builder_location
+    end
+    
+    def location
+    end
+
+    def materials
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
+  get 'builder', to: 'static_pages#builder'
+  get 'builder_location', to: 'static_pages#builder_location'
+  get 'location', to: 'static_pages#location'
+  get 'materials', to: 'static_pages#materials'
+  
+  
   root to: 'static_pages#home'
 end
